1b96e5e441cb9034ab4ff5e86b731443d6947686
Chulabhaya Wijesundara <chulabhayawijesundara@gmail.com>
2023-01-28 18:04:08 -0500

    refactor: Documentation update

--- sac_discrete_obs_discrete_action_recurrent.py
+++ sac_discrete_obs_discrete_action_recurrent.py
@@ -28,7 +28,7 @@ def parse_args():
     parser.add_argument("--wandb-project-name", type=str, default="sac-discrete-obs-discrete-action-recurrent",
         help="the wandb's project name")
     parser.add_argument("--wandb-dir", type=str, default="./",
-        help="the wandb's project name")
+        help="the wandb directory")
     parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
